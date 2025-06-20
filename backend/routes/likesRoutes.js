3c1c2c0dcb926c67763a028fb9edb76e14f16c25
ganeshakula80 <ganiakula80@gmail.com>
2025-06-20 21:22:42 +0530

    Delete backend directory

--- backend/routes/likesRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { toggleLike } = require("../controllers/likesController");
-const { getLikes } = require("../controllers/likesController");
-
-router.post("/", toggleLike);
-router.get("/:blogId", getLikes);
-
-module.exports = router;
